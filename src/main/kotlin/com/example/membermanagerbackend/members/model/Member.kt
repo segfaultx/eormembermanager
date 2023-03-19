159b9dab21a034f474404f4c5d3ade9a65e7d2b3
Amatus muscheites <amatus.muscheites@gmail.com>
2023-03-19 12:05:10 +0100

    add joined dates and promotion dates to member

--- src/main/kotlin/com/example/membermanagerbackend/members/model/Member.kt
+++ src/main/kotlin/com/example/membermanagerbackend/members/model/Member.kt
@@ -2,6 +2,7 @@ package com.example.membermanagerbackend.members.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
 
 @Document
 data class Member(
@@ -10,5 +11,7 @@ data class Member(
     val name: String,
     val server: String,
     var avatarUri: String = "",
-    var jobInfos: MutableSet<MemberJobInfo> = mutableSetOf()
+    var jobInfos: MutableSet<MemberJobInfo> = mutableSetOf(),
+    var joinedDate: LocalDate,
+    var promotionDate: LocalDate? = null
 )
